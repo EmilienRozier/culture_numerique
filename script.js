80b8c1ced3198125a10a0a0539309476a3eeb705
EmilienRozier <emilienrozier@icloud.com>
2023-04-18 16:48:35 +0200

    0.1

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const menu = document.getElementById('smenu');
+const article = document.getElementById('article');
 const btn = document.querySelector('button');
 
-btn.addEventListener('click', () => {
+article.addEventListener('click', () => {
     menu.classList.toggle('active')
     if (menu.classList == 'active') {
         btn.style.transform = 'rotate(90deg)'
